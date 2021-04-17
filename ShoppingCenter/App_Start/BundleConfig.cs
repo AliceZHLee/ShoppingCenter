3afb73febd291c88dbd81240fb852071d697f80d
AliceZHLee <lizhengyi135@gmail.com>
2021-04-18 01:21:42 +0800

    update

--- ShoppingCenter/App_Start/BundleConfig.cs
+++ ShoppingCenter/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace ShoppingCenter
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-flaty.css",
+                    //  "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
